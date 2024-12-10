aa8293ec6ee31f5353c34faaa4f72594a31018a5
Yusuf260511 <sultvnhuzaevusufhuza@gmail.com>
2024-12-10 20:32:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let userName = prompt('Ismingizni kiriting:');
+console.log(`Ism: ${userName}`);
+
+let userSurname = prompt('Familiyangizni kiriting:');
+console.log(`Familiya: ${userSurname}`);
+
+let userAge = prompt('Yoshingizni kiriting:');
+console.log(`Yosh: ${userAge}`);
+
+let userSubject = prompt('Yoqtirgan faningizni yozing:');
+console.log(`Yoqtirgan fan: ${userSubject}`);
+
+let userDistrict = prompt('Yashayotgan tumaningizni kiriting:');
+console.log(`Yashash manzili: ${userDistrict}`);
